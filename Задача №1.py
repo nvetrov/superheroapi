2135e5bf6330aca81f0f654e4d6ba2484be0a831
nvetrov <nvetrov@gmail.com>
2021-03-22 23:55:08 +0300

    done

--- Задача №1.py
+++ Задача №1.py
@@ -1,12 +1,14 @@
 from pprint import pprint
 import requests
 
+your_token = "2619421814940190"
 
-def get_data_hero(search_hero):
+
+def get_data_hero(search_hero, token):
     hero_dict = dict()
     for name_hero in search_hero:
         url = f' https://superheroapi.com/api/'
-        token = "2619421814940190/"
+
         full_url = url + token + "/search/" + name_hero
         r = requests.get(full_url)
         # print(r.url)
@@ -20,19 +22,27 @@ def get_data_hero(search_hero):
                     d['id'] = current_hero['id']
                     d['name'] = current_hero['name']
                     d['intelligence'] = current_hero['powerstats']['intelligence']
+                    # d['race'] = current_hero['appearance']['race']
+                    # d['strength'] = current_hero['powerstats']['strength']
+                    # d['speed'] = current_hero['powerstats']['speed']
+                    # d['durability'] = current_hero['powerstats']['durability']
+                    # d['power'] = current_hero['powerstats']['power']
+                    # d['combat'] = current_hero['powerstats']['combat']
+
                     hero_lst.append(d)
                     hero_dict[name_hero] = hero_lst
-        except KeyError as e:
+
+        except KeyError:
             print(f' Персонажа  {name_hero} нет')
 
     return hero_dict
 
 
 if __name__ == '__main__':
-    name = ['Hulk', 'Captain America', 'Thanos']
-    hero_result = get_data_hero(name)
+    name = ['Hulk', 'Captain America', 'Thanos', ]  # 'Galactus', 'Star-Lord' 'Captain Marvel'
+    hero_result = get_data_hero(search_hero=name, token=your_token + '/')
     max = 0
-    for  v in hero_result.values():
+    for v in hero_result.values():
         if max > int(v[0]['intelligence']):
             continue
         else:
@@ -41,5 +51,3 @@ if __name__ == '__main__':
     for k, v in hero_result.items():
         if max == int(v[0]['intelligence']):
             print(k, max)
-            break
-
